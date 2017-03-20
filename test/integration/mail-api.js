da6a9cfcd037840cdffe4caf8bd45cae4c5c6077
Wonyoung Ju <getogrand@gmail.com>
2017-03-20 23:32:06 +0900

    Assert mail data on list api and refactor

--- test/integration/mail-api.js
+++ test/integration/mail-api.js
@@ -2,8 +2,9 @@
 
 const request = require('supertest');
 const factory = require('factory-girl').factory;
+const moment = require('moment');
 const _ = require('lodash');
-require('should');
+const should = require('should');
 const Mail = require('../../models/mail');
 
 const app = require('../../app');
@@ -15,6 +16,21 @@ const assertMailField = (field, actual, expected) => {
     oneOfActual.address.should.equal(oneOfExpected.address);
     oneOfActual.name.should.equal(oneOfExpected.name);
   });
+};
+
+const assertMail = (actual, expected) => {
+  actual.subject.should.equal(expected.subject);
+  actual.recipient.should.equal(expected.recipient);
+  // nullable assertion
+  should(actual.secretCode).equal(expected.secretCode);
+  moment(actual.date).isSame(moment(expected.date)).should.be.true();
+  actual.messageId.should.equal(expected.messageId);
+  actual.text.should.equal(expected.text);
+  actual.html.should.equal(expected.html);
+  assertMailField('to', actual, expected);
+  assertMailField('from', actual, expected);
+  assertMailField('cc', actual, expected);
+  assertMailField('bcc', actual, expected);
 };
 
 describe('POST /api/mails/create', function() {
@@ -30,18 +46,7 @@ describe('POST /api/mails/create', function() {
 
       return Mail.findById(res.body.mailId);
     }).then((saved) => {
-      saved.subject.should.equal(expected.subject);
-      saved.recipient.should.equal(expected.recipient);
-      saved.secretCode.should.equal(expected.secretCode);
-      // You should use eql instead of equal for assertion of Date object.
-      saved.date.should.eql(expected.date);
-      saved.messageId.should.equal(expected.messageId);
-      saved.text.should.equal(expected.text);
-      saved.html.should.equal(expected.html);
-      assertMailField('to', saved, expected);
-      assertMailField('from', saved, expected);
-      assertMailField('cc', saved, expected);
-      assertMailField('bcc', saved, expected);
+      assertMail(saved, expected);
     }).then(done).catch(done);
   });
 });
@@ -63,6 +68,9 @@ describe('GET /api/mails', function() {
     request(app).get(`/api/mails?recipient=${fixture.recipient}`).expect(200).then((res) => {
       const mails = res.body;
       mails.length.should.equal(3);
+      mails.forEach((m, i) => {
+        assertMail(m, fixture.mails[i]);
+      });
     }).then(done).catch(done);
   });
 });
