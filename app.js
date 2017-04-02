6d3f59125bfaf46aa59963934a750af9f50a7dbb
Wonyoung Ju <getogrand@gmail.com>
2017-04-02 14:47:24 +0900

    Supress morgan http logger in test environment

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+  app.use(logger('dev'));
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
